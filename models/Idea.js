00d70cecb3551eb0a2c81857334b8b1d768cde6b
saurajb <saurajbabu@gmail.com>
2024-08-29 12:19:40 +0530

    fixes

--- models/Idea.js
+++ models/Idea.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const ideaSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: ['true', 'please add a text field'],
+  },
   text: {
     type: String,
     required: ['true', 'please add a text field'],
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./config/db');
 
 connectDB();
 
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 5600;
 
 const app = express();
 
@@ -14,6 +15,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//CORS middleware
+app.use(
+  cors({
+    orgin: ['http://localhost:3000', 'http://localhost:5600'],
+    credentials: true,
+  })
+);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the RandomIdeas API' });
